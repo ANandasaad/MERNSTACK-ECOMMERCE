6fba7ddd9b6dbb3f54be0e3ccfecedf3368aeb03
Anand kushwaha <anandkushwaha70@gmail.com>
2022-10-26 16:36:24 +0530

    User Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,8 +18,42 @@ exports.registerUser=catchAsyncErrors(async(req,res,next)=>{
         },
     });
 
+    const token =user.getJWTToken();
+
     res.status(201).json({
         success:true,
-        user,
+        token,
     });
 });
+
+//Login User
+
+exports.loginUser=catchAsyncErrors(async(req,res,next)=>{
+    const {email,password}= req.body;
+
+    //checking if user has given password and email both
+
+    if(!email || !password)
+    {
+        return next(new ErrorHandler("Please Enter the Password",400));
+    }
+
+    const user=User.findOne({email}).select("+password");
+
+    if(!user){
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+
+    const isPasswordMatched =user.comparePassword();
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+
+    const token =user.getJWTToken();
+
+    res.status(200).json({
+        success:true,
+        token,
+    });
+})
